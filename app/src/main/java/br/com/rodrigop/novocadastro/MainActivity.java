c99f0fa14c6f2e1edc5e075a13f35acc389f7bda
rodrigo passos <contato@rodrigop.com.br>
2016-02-27 16:42:48 -0300

    Adicionado telas

--- app/src/main/java/br/com/rodrigop/novocadastro/MainActivity.java
+++ app/src/main/java/br/com/rodrigop/novocadastro/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.rodrigop.novocadastro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNome,
+            editTextIdade,
+            editTextTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        // Captura campos do layout
+        editTextNome = (EditText) findViewById(R.id.campoNome);
+        editTextTelefone = (EditText) findViewById(R.id.campoTelefone);
+        editTextIdade = (EditText) findViewById(R.id.campoIdade);
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String nome = editTextNome.getText().toString();
+                String idade = editTextIdade.getText().toString();
+                String telefone = editTextTelefone.getText().toString();
+
+                // Inicia objeto que irá realizar a troca de tela
+                Intent intent = new Intent(MainActivity.this, Tela2Activity.class);
+
+                intent.putExtra(nome, R.id.labelNome);
             }
         });
     }
---
+++ app/src/main/java/br/com/rodrigop/novocadastro/Tela2Activity.java
@@ -0,0 +1,27 @@
+package br.com.rodrigop.novocadastro;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Tela2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+    public void setValores(String nome, String idade, String telefone) {
+
+    }
+
+}
